d8bd1c05ce735e5610ddf9b68ec3b88e5ab54ecd
Edwin Forss <eddan.forss@gmail.com>
2021-12-19 16:59:29 +0100

    meny

---
+++ Movieprodjekt/Menu1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movieprodjekt
+{
+    class Menu1
+    {
+        public static void PrintFirstMenu()
+        {
+            Console.WriteLine("Välj vad du vill göra:");
+            Console.WriteLine("1)Sök efter film med ID");
+            Console.WriteLine("2)avsluta pogrammet");
+
+        }
+
+        public static void UserChoice()
+        {
+            float x = float.Parse(Console.ReadLine());
+            switch(x)
+            {
+                case '1':
+
+                    Movie();
+
+
+                    break;
+
+                case '2':
+
+                    
+                    
+                    break;
+            }
+        }
+        
+    }
+
+
+}
