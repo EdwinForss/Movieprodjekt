ed21d36d35da62b691c1dceff1d5768502130c75
Edwin Forss <eddan.forss@gmail.com>
2021-12-20 14:33:34 +0100

    fix på api

--- Movieprodjekt/APIMovie.cs
+++ Movieprodjekt/APIMovie.cs
@@ -1,10 +1,12 @@
-﻿using Newtonsoft.Json;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 
 namespace Movieprodjekt
 {
